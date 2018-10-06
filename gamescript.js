ecaa9c0d83a9b9054342da2ee9a0941cc843d0df
jasonnowak <jason.nowak123@gmail.com>
2018-10-06 14:36:57 -0400

    Commiting js update for merch page order calculator, slideshow, canvas and extra js game for home page.

--- gamescript.js
+++ gamescript.js
@@ -10,4 +10,20 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     }
+    var myGamePiece;
+
+    function startGame() {
+        myGameArea.start();
+        myGamePiece = new component(30, 30, "gold", 10, 120);
+    }
+
+    function component(width, height, color, x, y) {
+        this.width = width;
+        this.height = height;
+        this.x = x;
+        this.y = y;
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillCircle(this.x, this.y, this.width);
+    }
 }
